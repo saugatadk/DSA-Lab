cb1ff0cef522c7a789f86b06c303ed25afef078d
saugatadk <adhikarisaugat55@gmail.com>
2025-02-03 16:55:27 +0545

    Add: Binary Search

--- lab7/binarySearch.cpp
+++ lab7/binarySearch.cpp
@@ -1,41 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-int binarySearch(vector<int>& arr, int target) {
+int main(){
+    vector<int> arr ={2,3,4,10,40};
+    int target = 10;
+    
     int left = 0;
     int right = arr.size() - 1;
-
-    while (left <= right) {
+    int location = -1;
+    while(left <= right){
         int mid = left + (right - left) / 2;
 
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
+        if(arr[mid] == target){
+            location = mid;
+            break;
+        }else if(arr[mid] < target){
             left = mid + 1;
-        } else {
+        }else{
             right = mid - 1;
         }
     }
-
-    return -1;
-}
-
-int main() {
-    vector<int> arr = {2, 3, 4, 10, 40};
-    int target = 10;
-
-    sort(arr.begin(), arr.end());
-
-    int result = binarySearch(arr, target);
-
-    if (result != -1) {
-        cout << "Element found at index: " << result << endl;
-    } else {
+    
+    if(location != -1){
+        cout << "Element found at index: " << location << endl;
+    }else{
         cout << "Element not found in the array." << endl;
     }
 
-    return 0;
+
 }
