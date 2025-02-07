b0bb801525d61078175b2c85bb753d51e05c1f70
saugatadk <adhikarisaugat55@gmail.com>
2025-02-07 08:58:37 +0545

    ADD: insertion and selection Sort

---
+++ lab8/insertionSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void insertionSort(vector<int>& arr){
+    int n = arr.size();
+    for(int i = 1; i < n; i++){
+        int curr = arr[i];
+        int prev = i - 1;
+        while(prev >= 0 && arr[prev] > curr){
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr;
+    }
+}
+void printArray(vector<int>& arr){
+    for(int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    vector<int> arr = {4, 1, 5, 2, 3};
+    insertionSort(arr);
+    printArray(arr);
+    return 0;
+}
